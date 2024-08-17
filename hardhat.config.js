2ac562a49596f91f9327d64cb1f61024c7036272
Your Name <youremail@gmail.com>
2024-08-17 09:05:32 +0200

    nft721

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+//use configuration-variables in hardhat to set PRIVATE_KEY variable
+const PRIVATE_KEY = "";
+
+module.exports = {
+  defaultNetwork: "swisstronik",
+  solidity: "0.8.20",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x` + `${PRIVATE_KEY}`],
+    },
+  },
+};
